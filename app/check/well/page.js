788d7a30513077c6440aa38acb160fee1a1e743b
lengsukq <lengsukq@foxmail.com>
2023-11-21 17:30:28 +0800

    井字棋

--- app/check/well/page.js
+++ app/check/well/page.js
@@ -3,9 +3,15 @@ import { useState,useRef } from 'react';
 import './page.css'
 export default function Board() {
     const [squares, setSquares] = useState(Array(9).fill(null));
+    const [result, setResult] = useState(false);
+
     const squaresRef = useRef(squares)
     const [xIsNext, setXIsNext] = useState(true);
     function handleClick(i) {
+        if (result){
+            console.log('游戏结束');
+            return false;
+        }
         console.log('i---',i)
         const newSquares = squares.slice();
         newSquares[i] = xIsNext?'X':'O';
@@ -16,16 +22,13 @@ export default function Board() {
         over();
     }
     function over(){
-            let result = calculateWinner(squaresRef.current);
-            console.log('result',result)
-            if (result==='over'){
-                // alert('游戏结束')
-            }
+        setResult(calculateWinner(squaresRef.current) === 'over'?true:false);
 
     }
 
     return (
         <>
+            <h5>{!result?"下一步":"胜者"}：{xIsNext?'X':'O'}</h5>
             <div className="board-row">
                 <Square value={squares[0]} onSquareClick={()=>handleClick(0)}/>
                 <Square value={squares[1]} onSquareClick={()=>handleClick(1)}/>
@@ -62,8 +65,9 @@ function calculateWinner(squares) {
     for (let i = 0; i < lines.length; i++) {
         const [a, b, c] = lines[i];
         if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            console.log('已有赢家')
             return 'over';
         }
     }
-    return null;
+    return '';
 }
