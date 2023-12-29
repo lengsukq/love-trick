0a3a4f283e9d18a88a38753eea5e1ff0c8c6bed2
lengsukq <lengsukq@foxmail.com>
2023-12-29 17:11:01 +0800

    添加local

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    env: {
+        DB_HOST: process.env.DB_HOST,
+        DB_USER: process.env.DB_USER,
+        DB_PASSWORD: process.env.DB_PASSWORD,
+        DB_DATABASE: process.env.DB_DATABASE,
+    },
 }
 
 module.exports = nextConfig
