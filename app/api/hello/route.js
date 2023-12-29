39da7f18a2b838b77c3f00a0fd2a7ac0a24be641
Su <lengsukq@foxmail.com>
2023-12-29 21:08:40 +0800

    登录接口demo

--- app/api/hello/route.js
+++ app/api/hello/route.js
@@ -1,5 +1,16 @@
 import BizResult from '@/app/utils/BizResult';
 
+
+export async function HEAD(request) {}
+
+export async function PUT(request) {}
+
+export async function DELETE(request) {}
+
+export async function PATCH(request) {}
+
+// If `OPTIONS` is not defined, Next.js will automatically implement `OPTIONS` and  set the appropriate Response `Allow` header depending on the other methods defined in the route handler.
+export async function OPTIONS(request) {}
 export async function GET(req) {
     const {searchParams} = new URL(req.url)
     console.log('searchParams', searchParams)
