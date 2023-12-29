379a94940afca5b9ffb21d1d9c24705154c2e696
lengsukq <lengsukq@foxmail.com>
2023-12-29 21:08:40 +0800

    登录接口demo

--- app/api/user/route.js
+++ app/api/user/route.js
@@ -2,6 +2,11 @@ import BizResult from '@/app/utils/BizResult';
 import executeQuery from "@/app/utils/db";
 
 export async function GET(req) {
+    const {searchParams} = new URL(req.url)
+    const username = searchParams.get('username')
+    const password = searchParams.get('password')
+    console.log('searchParams', searchParams,'username---',username,'password---',password)
+
     let info = null;
     try {
         console.log("req nom", req.body)
--- app/page.js
+++ app/page.js
@@ -1,10 +1,42 @@
-import React from "react";
-import {Button} from '@nextui-org/button';
+'use client'
+import React, {useEffect, useState} from "react";
+import {Input, Button, Avatar} from "@nextui-org/react";
 
 export default function Home() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const login = async () => {
+        console.log('用户名:', username);
+        console.log('密码:', password);
+        try {
+            const response = await fetch(`/api/user?username=${username}&password=${password}`, {
+                method: 'get',
+                });
+            if (response.ok) {
+                const data = await response.json();
+                console.log('data', data)
+            } else {
+                throw new Error('请求失败');
+            }
+        } catch (error) {
+            console.error('请求错误:', error);
+        }
+
+    };
+
+
     return (
-    <h1 className="text-3xl font-bold underline">
-        <Button color="primary">按钮</Button>
-    </h1>
+        <div className="container flex flex-col justify-center items-center px-4 h-lvh">
+            <Avatar isBordered radius="full" src="/defaultAvatar.jpg" className="w-40 h-40 text-large"/>
+            <div className="flex w-full flex-wrap md:flex-nowrap gap-4 m-10">
+                <Input type="text" label="昵称" placeholder="请输入昵称" value={username}
+                       onChange={(e) => setUsername(e.target.value)}/>
+                <Input type="password" label="密码" placeholder="请输入密码" value={password}
+                       onChange={(e) => setPassword(e.target.value)}/>
+            </div>
+            <Button color="primary" className="" onClick={login}>
+                登录
+            </Button>
+        </div>
     )
 }
