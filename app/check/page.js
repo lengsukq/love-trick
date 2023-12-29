c36fe82a7cfff05f077ad1448d70e80df05e7707
lengsukq <lengsukq@foxmail.com>
2023-12-29 15:56:01 +0800

    引入next ui

--- app/check/page.js
+++ app/check/page.js
@@ -1,14 +1,14 @@
-import Image from 'next/image'
 import React from "react";
-import {Button} from "antd";
+import {User} from "@nextui-org/react";
 
-
-export default function Home() {
-
-
-  return (
-      <div className="App">
-          <Button type="primary">Button</Button>
-      </div>
-  )
+export default function App() {
+    return (
+        <User
+            name="Jane Doe"
+            description="Product Designer"
+            avatarProps={{
+                src: "https://i.pravatar.cc/150?u=a04258114e29026702d"
+            }}
+        />
+    );
 }
