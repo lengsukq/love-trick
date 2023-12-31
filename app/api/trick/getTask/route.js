65562114427c8da8980d647b98019bcac4d4f18e
Su <lengsukq@foxmail.com>
2023-12-31 19:54:04 +0800

    修改为获取lover和自己的任务

--- app/api/trick/getTask/route.js
+++ app/api/trick/getTask/route.js
@@ -5,10 +5,15 @@ import {parseCookie} from "@/app/utils/parseCookie";
 export async function GET(req) {
     const cookieInfo = parseCookie(req);
     try {
+        const userInfo = await executeQuery({
+            // 查询有无此用户
+            query: 'SELECT lover FROM userinfo WHERE userEmail = ?',
+            values: [cookieInfo.name]
+        });
         const result = await executeQuery({
             // 查询任务列表
-            query: 'SELECT * FROM tasklist WHERE publisherEmail = ? OR receiverEmail = ? ORDER BY taskId DESC',
-            values: [cookieInfo.name, cookieInfo.name]
+            query: 'SELECT * FROM tasklist WHERE publisherEmail = ? OR publisherEmail = ? OR  receiverEmail = ? ORDER BY taskId DESC',
+            values: [cookieInfo.name, userInfo[0].lover, cookieInfo.name]
         });
         // console.log('result',result)
         result.forEach(item => {
