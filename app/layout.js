94cd7ea41240e41c3dd859d35b7d548cdff6c125
lengsukq <lengsukq@foxmail.com>
2024-01-03 09:53:26 +0800

    添加部分标题

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,10 @@
 // app/layout.tsx
 import {Providers} from "./providers";
 import './globals.css'
+export const metadata = {
+    title: '登录 | love-trick',
+    description: 'love-trick',
+};
 export default function RootLayout({children}) {
     return (
         <html lang="en" className='light'>
---
+++ app/trick/taskInfo/layout.js
@@ -0,0 +1,8 @@
+import React from "react";
+export const metadata  = {
+    title: '任务详情',
+    description: 'love-trick',
+}
+export default function TaskInfoLayout({children}) {
+    return <section>{children}</section>
+}
