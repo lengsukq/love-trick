c75ead73b3c84d7a4913cd8c5d26cb45133c484c
lengsukq <lengsukq@foxmail.com>
2024-01-11 17:07:39 +0800

    添加nextui快速导航，修改只在三个主页面中显示全局导航组件

--- app/trick/trickProviders.jsx
+++ app/trick/trickProviders.jsx
@@ -3,34 +3,47 @@ import React from "react";
 import {useEffect, useState} from 'react'
 
 import {Tabs, Tab, Button} from "@nextui-org/react";
+// import {usePathname} from "next/navigation";
 import {useRouter, usePathname} from 'next/navigation'
 
-export function TrickProviders({children}) {
+// 全局组件
+const GlobalComponent = () => {
     const router = useRouter()
+    // 获取路由信息
     const pathname = usePathname();
-    useEffect(() => {
-        console.log('trickPro---', pathname)
-    }, [])
+    console.log('pathname',pathname)
     const toPage = (key) => {
         console.log('toPage', key)
-        const pathObj = {
-            "home": "/trick",
-            "postTask": "/trick/postTask",
-            "myInfo": "/trick/myInfo"
-        }
-        router.push(pathObj[key])
+        // const pathObj = {
+        //     "home": "/trick",
+        //     "postTask": "/trick/postTask",
+        //     "myInfo": "/trick/myInfo"
+        // }
+        router.push(key)
 
     }
+    // 只在路由为 "/trick" 时显示组件
+    if (pathname === '/trick' || pathname === '/trick/postTask' || pathname === '/trick/myInfo') {
+        return (
+            <div className="flex flex-wrap gap-4 w-full justify-center fixed bottom-10 z-10000">
+                <Tabs selectedKey={pathname} key="lg" size="lg" aria-label="Options" onSelectionChange={(e) => toPage(e)}>
+                    <Tab key="/trick" title="首页"/>
+                    <Tab key="/trick/postTask" title="发布"/>
+                    <Tab key="/trick/myInfo" title="我的"/>
+                </Tabs>
+            </div>
+        );
+    }
+
+    // 在其他路由下不显示组件
+    return null;
+};
+
+export function TrickProviders({children}) {
     return (
         <div className="h-lvh">
             {children}
-            <div className="flex flex-wrap gap-4 w-full justify-center fixed bottom-10 z-10000">
-                <Tabs key="lg" size="lg" aria-label="Tabs sizes" onSelectionChange={(e) => toPage(e)}>
-                    <Tab key="home" title="首页"/>
-                    <Tab key="postTask" title="发布"/>
-                    <Tab key="myInfo" title="我的"/>
-                </Tabs>
-            </div>
+            <GlobalComponent />
         </div>
 
 
