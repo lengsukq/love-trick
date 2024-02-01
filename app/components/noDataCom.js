38aea0cef093c7bda69c9731e0c4210b135cebe3
lengsukq <lengsukq@foxmail.com>
2024-02-01 14:13:08 +0800

    样式优化

--- app/components/noDataCom.js
+++ app/components/noDataCom.js
@@ -5,7 +5,7 @@ export default function NoDataCom({}) {
     return (
         <div className={"h-full flex flex-col justify-center items-center"}>
             <NoData/>
-            <p>暂时还没有数据哦</p>
+            <p className={"mt-5 text-gray-600"}>暂时还没有数据哦</p>
         </div>
     )
 };
