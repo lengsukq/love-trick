66fac1c3cf31d43a38750377b93ca632d347b2bf
lengsukq <lengsukq@foxmail.com>
2024-02-02 09:26:45 +0800

    收藏语句，查询列表和收藏功能

---
+++ app/api/favourite/getFav/route.js
@@ -0,0 +1,27 @@
+'use server'
+import BizResult from "@/app/utils/BizResult";
+import {cookieTools} from "@/app/utils/cookieTools";
+import executeQuery from "@/app/utils/db";
+
+export async function POST(req) {
+    try {
+        const {userEmail} = cookieTools(req);
+        const jsonData = await req.json();
+        const {type} = jsonData;
+        const typeObj = {'gift':"gift_list",'task':'tasklist','whisper':'whisper_list'};
+        if (!typeObj.hasOwnProperty(type)){
+            return Response.json(BizResult.fail('', '收藏类型错误'))
+        }
+        console.log('type',typeObj[type])
+
+        const result = await executeQuery({
+            // 插入任务数据
+            query: `SELECT favourite_list.*,${typeObj[type]}.* FROM favourite_list JOIN ${typeObj[type]} ON collectionId = ${typeObj[type]}.${type+"Id"} WHERE userEmail = ?`,
+            values: [userEmail]
+        });
+        return Response.json(BizResult.success(result, '查询成功'))
+    } catch (error) {
+        console.log(error);
+        return Response.json(BizResult.fail('', '系统异常'))
+    }
+}
