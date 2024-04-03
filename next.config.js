2990101ffc0b12257c18b611a9d038a4961cdfdb
lengsukq <lengsukq@foxmail.com>
2024-04-03 14:17:28 +0800

    尝试添加docker支持

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: "standalone",
     webpack: (config, {isServer}) => {
         // 禁用代码压缩
         config.optimization.minimize = false;
