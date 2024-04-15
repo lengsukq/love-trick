17e87e0e40d89fb9ffcaf9d146f985163c068feb
lengsukq <lengsukq@foxmail.com>
2024-04-15 08:20:31 +0800

    身份验证失败或者登录过期清除无效cookie，用户切换页面自动跳转登录页

--- middleware.js
+++ middleware.js
@@ -3,7 +3,6 @@ import {NextResponse} from 'next/server'
 import {cookies} from 'next/headers'
 import dayjs from "dayjs";
 import BizResult from "@/app/utils/BizResult";
-import {redirect} from 'next/navigation'
 
 export function middleware(request) {
     try {
@@ -16,16 +15,23 @@ export function middleware(request) {
         // console.log('clientCookie', clientCookie)
         const serverCookie = cookies().get(clientCookie.name);
         // console.log('serverCookie', serverCookie);
+        const pastDate = new Date(Date.now() - 86400000).toUTCString();
 
         if (clientCookie.value !== serverCookie.value) {
 
-            return Response.json(BizResult.fail('', '身份验证失败，请重新登录'))
+            return Response.json(BizResult.fail('', '身份验证失败，请重新登录'),{
+                status: 200,
+                headers: {'Set-Cookie': `cookie=;Expires=${pastDate};`},
+            })
         }
         const cookieDate = dayjs(clientCookie.expires);
         if (cookieDate.isAfter(dayjs())) {
             return NextResponse.next()
         } else {
-            return Response.json(BizResult.fail('', '登录过期'))
+            return Response.json(BizResult.fail('', '登录过期'),{
+                status: 200,
+                headers: {'Set-Cookie': `cookie=;Expires=${pastDate};`},
+            })
         }
     }catch (e){
         console.log('中间件报错:',e);
